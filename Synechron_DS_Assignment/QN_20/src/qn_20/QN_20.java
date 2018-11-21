0a7a9e9a88887d3a06f2b884d22d5783fd6ee940
Vidyakamal <31116133+VidyakamalNSIT@users.noreply.github.com>
2018-11-21 14:18:29 +0530

    Update QN_20.java

--- Synechron_DS_Assignment/QN_20/src/qn_20/QN_20.java
+++ Synechron_DS_Assignment/QN_20/src/qn_20/QN_20.java
@@ -3,14 +3,37 @@
  */
 package qn_20;
 
-
-public class QN_20 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+import java.util.Scanner;
+class QN_20{ 
+    void search(int[][] mat, int n, int x) { 
+		int i = 0, j = n-1;
+		while ( i < n && j >= 0 ) 
+		{ 
+			if ( mat[i][j] == x ) 
+			{ 
+				System.out.print("n Found at "+ i + " " + j); 
+				return; 
+			} 
+			if ( mat[i][j] > x ) 
+				j--; 
+			else 
+				i++; 
+		} 
+		
+		System.out.print("n Element not found"); 
+		
+	} 
+		public static void main(String[] args) { 
+		Scanner sc = new Scanner(System.in);
+        int R = sc.nextInt(); 
+        int C = sc.nextInt(); 
+        int[][] arr = new int[3][6];
+        for(int i=0;i<R;i++){
+            for(int j=0; j<C; j++)
+                arr[i][j] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        QN_20 q = new QN_20();
+        q.search(arr,C,k);
+	} 
 }
