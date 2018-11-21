937cc9e4b2f9eaa7afa3b7a9368aca679e0cf5ec
Vidyakamal <31116133+VidyakamalNSIT@users.noreply.github.com>
2018-11-21 11:53:26 +0530

    Update QN_18_Queue.java

--- Synechron_DS_Assignment/QN_18_Queue/src/qn_18_queue/QN_18_Queue.java
+++ Synechron_DS_Assignment/QN_18_Queue/src/qn_18_queue/QN_18_Queue.java
@@ -9,13 +9,91 @@ package qn_18_queue;
  *
  * @author Vidyakamal
  */
-public class QN_18_Queue {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+import java.util.Scanner;
+public class QN_18_Queue  {
+	
+    int front=0,rear=-1,size=0;
+    int capacity;
+    int arr[];
+    
+    QN_18_Queue(int capacity)
+    {
+        this.capacity=capacity;
+       
+        arr=new int[capacity];
+    }
+    public boolean isQueueFull(){
+        boolean status = false;
+        if (size == capacity){
+            status = true;
+        }
+        return status;
+    }
+    public void enqueue(int data)
+    {
+        if(isQueueFull())
+        {
+            System.out.println("overflow");
+        }
+        else{
+            rear++;
+           
+            arr[rear] = data;
+            size++;
+            
+        
+        }
+    }
+     public boolean isQueueEmpty(){
+        boolean status = false;
+        if (size == 0){
+            status = true;
+        }
+        return status;
+    }
+    public void dequeue() {
+        if (isQueueEmpty()) {
+            System.out.println("Underflow ");
+        } else {
+            front++;
+            if(front == capacity-1){
+                System.out.println("Pop operation done, removed: "+arr[front-1]);
+                front = 0;
+            } else {
+                System.out.println("Pop operation done, removed: "+arr[front-1]);
+            }
+            size--;
+        }
+    }
+    public  void printQueue()
+    {
+        if(isQueueEmpty())
+        {
+            System.out.println("queue is empty");
+        }
+       else
+        {
+            for(int i=front;i<=rear;i++)
+            {
+                System.out.print(arr[i]+" ");
+            }
+        }
+       
+        System.out.println();
+        
+    }
+    
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the size of queue");
+        int l=sc.nextInt();
+        QN_18_Queue q=new QN_18_Queue(l);
+        System.out.println("enter the data into queue");
+        for(int i=0;i<l;i++){
+            q.enqueue(sc.nextInt());
+        }
+        q.printQueue();
+        
     }
     
 }
