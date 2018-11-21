ec235bc25753ab404497e3265e29095b77d640bb
Vidyakamal <31116133+VidyakamalNSIT@users.noreply.github.com>
2018-11-21 11:09:49 +0530

    Update QN_2.java

--- Synechron_DS_Assignment/QN_2/src/qn_2/QN_2.java
+++ Synechron_DS_Assignment/QN_2/src/qn_2/QN_2.java
@@ -4,18 +4,78 @@
  * and open the template in the editor.
  */
 package qn_2;
+/*
+========================================================
+//2. Program for nth node from the end of a Linked List
+========================================================
+*/
+import java.util.*;
+class Node
+{
+  int data;
+  Node next;
+    Node (int data)
+  {
+    this.data = data;
+    this.next = null;
+  }
+}
+class QN_2 {
+    Node head;
+  private int length;
 
-/**
- *
- * @author Vidyakamal
- */
-public class QN_2 {
+    QN_2 ()
+  {
+    this.length = 0;
+  }
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+  QN_2 (int data)
+  {
+    this.head = new Node (data);
+    length++;
+  }
+  public void add (int data)
+  {
+    Node node = new Node (data);
+    if (head == null)
+      {
+	head = node;
+	length++;
+      }
+    else
+      {
+	Node temp = head;
+	while (temp.next != null)
+	  temp = temp.next;
+	temp.next = node;
+	length++;
+      }
+  }
+  
+  public void Nth_from_last(int n){
+      Node f_count = head;
+      Node l_count = head;
+      try {
+      while(n-->0){
+              l_count = l_count.next;
+          }
+      while(l_count!=null){
+          l_count = l_count.next;
+          f_count = f_count.next;
+      }
+      System.out.println("Nth element from last: "+f_count.data);
+      }catch(NullPointerException e) {
+        System.out.println("LinkedList Have Less Element");
     }
-    
+  }
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int len = sc.nextInt();
+		QN_2 list = new QN_2();
+		while(len-->0){
+		    list.add(sc.nextInt());
+		}
+		System.out.println("Wnat Data At: ");
+		list.Nth_from_last(sc.nextInt());
+	}
 }
