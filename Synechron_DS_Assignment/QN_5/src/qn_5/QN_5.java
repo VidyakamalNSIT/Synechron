ff0d16191c4aa988c40f20acf99fd3be78ebbae0
Vidyakamal <31116133+VidyakamalNSIT@users.noreply.github.com>
2018-11-21 11:17:32 +0530

    Update QN_5.java

--- Synechron_DS_Assignment/QN_5/src/qn_5/QN_5.java
+++ Synechron_DS_Assignment/QN_5/src/qn_5/QN_5.java
@@ -6,16 +6,44 @@
 package qn_5;
 
 /**
- *
+ * Find third largest number in array without sorting in java
  * @author Vidyakamal
  */
-public class QN_5 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+import java.util.Scanner;
+class QN_5 { 
     
-}
+    void thirdLargest(int arr[],int size) { 
+	if (size < 3) { 
+		System.out.printf(" Invalid Input "); 
+		return; 
+	}  
+	int first = arr[0]; 
+	for (int i = 1; i < size ; i++) 
+		if (arr[i] > first) 
+			first = arr[i]; 
+	int second = Integer.MIN_VALUE; 
+	for (int i = 0; 
+			i < size ; i++) 
+		if (arr[i] > second && 
+			arr[i] < first) 
+			second = arr[i]; 
+	int third = Integer.MIN_VALUE; 
+	for (int i = 0;i < size ; i++) 
+		if (arr[i] > third && arr[i] < second) 
+			third = arr[i]; 
+	System.out.printf("Third Largest " + 
+				"element "+ third); 
+} 
+public static void main(String []args) 
+{ 
+	QN_5 obj = new QN_5(); 
+	Scanner sc = new Scanner(System.in);
+	int n=sc.nextInt();
+	int[] arr = new int[n];
+	for(int i=0; i<n; i++){
+	    arr[i]=sc.nextInt();
+	}
+	obj.thirdLargest(arr, n); 
+} 
+} 
+
