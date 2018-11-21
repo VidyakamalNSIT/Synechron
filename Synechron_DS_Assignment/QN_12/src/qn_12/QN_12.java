70c8432a77b7ed7efbe612e9e93c5f420b65e8da
Vidyakamal <31116133+VidyakamalNSIT@users.noreply.github.com>
2018-11-21 11:49:15 +0530

    Update QN_12.java

--- Synechron_DS_Assignment/QN_12/src/qn_12/QN_12.java
+++ Synechron_DS_Assignment/QN_12/src/qn_12/QN_12.java
@@ -9,13 +9,85 @@ package qn_12;
  *
  * @author Vidyakamal
  */
-public class QN_12 {
+// Java program to implement two stacks in a 
+// single array 
+class QN_12 
+{ 
+	int size; 
+	int top1, top2; 
+	int arr[]; 
+	QN_12(int n) 
+	{ 
+		arr = new int[n]; 
+		size = n; 
+		top1 = -1; 
+		top2 = size; 
+	} 
+	void push1(int x) 
+	{ 
+		if (top1 < top2 - 1) 
+		{ 
+			top1++; 
+			arr[top1] = x; 
+		} 
+		else
+		{ 
+			System.out.println("Stack Overflow"); 
+			System.exit(1); 
+		} 
+	} 
+	void push2(int x) 
+	{ 
+		if (top1 < top2 -1) 
+		{ 
+			top2--; 
+			arr[top2] = x; 
+		} 
+		else
+		{ 
+			System.out.println("Stack Overflow"); 
+			System.exit(1); 
+		} 
+	}  
+	int pop1() 
+	{ 
+		if (top1 >= 0) 
+		{ 
+			int x = arr[top1]; 
+			top1--; 
+			return x; 
+		} 
+		else
+		{ 
+			System.out.println("Stack Underflow"); 
+			System.exit(1); 
+		} 
+		return 0; 
+	}  
+	int pop2() 
+	{ 
+		if(top2 < size) 
+		{ 
+			int x =arr[top2]; 
+			top2++; 
+			return x; 
+		} 
+		else
+		{ 
+			System.out.println("Stack Underflow"); 
+			System.exit(1); 
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
-}
+		} 
+		return 0; 
+	} 
+	public static void main(String args[]) 
+	{ 
+		QN_12 ts = new QN_12(5); 
+		ts.push1(5); 
+		ts.push2(10); 
+		ts.push2(15); 
+		ts.push1(11); 
+		ts.push2(7);  
+	} 
+} 
+
