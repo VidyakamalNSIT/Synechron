6b465dce938a3f039185949c7413fcb8bb236fcd
Vidyakamal <31116133+VidyakamalNSIT@users.noreply.github.com>
2018-11-21 11:24:46 +0530

    Update QN_6.java

--- Synechron_DS_Assignment/QN_6/src/qn_6/QN_6.java
+++ Synechron_DS_Assignment/QN_6/src/qn_6/QN_6.java
@@ -6,16 +6,61 @@
 package qn_6;
 
 /**
- *
+ * Java program to sort an array of 0, 1 and 2 
  * @author Vidyakamal
  */
-public class QN_6 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
-}
+import java.util.Scanner; 
+
+class QN_6 {  
+    void Sort(int arr[], int arr_size) 
+	{ 
+		int lo = 0; 
+		int hi = arr_size - 1; 
+		int mid = 0,temp=0; 
+		while (mid <= hi) 
+		{ 
+			switch (arr[mid]) 
+			{ 
+			case 0: 
+			{ 
+				temp = arr[lo]; 
+				arr[lo] = arr[mid]; 
+				arr[mid] = temp; 
+				lo++; 
+				mid++; 
+				break; 
+			} 
+			case 1: 
+				mid++; 
+				break; 
+			case 2: 
+			{ 
+				temp = arr[mid]; 
+				arr[mid] = arr[hi]; 
+				arr[hi] = temp; 
+				hi--; 
+				break; 
+			} 
+			} 
+		} 
+	} 
+	void printArray(int arr[], int arr_size){ 
+		int i; 
+		for (i = 0; i < arr_size; i++) 
+			System.out.print(arr[i]+" "); 
+		System.out.println(""); 
+	} 
+	public static void main (String[] args) { 
+		QN_6 obj = new QN_6(); 
+    	Scanner sc = new Scanner(System.in);
+    	int n=sc.nextInt();
+    	int[] arr = new int[n];
+    	for(int i=0; i<n; i++){
+    	    arr[i]=sc.nextInt();
+    	}
+		obj.Sort(arr, n); 
+		System.out.println("Array after Sorting "); 
+		obj.printArray(arr, n); 
+	} 
+} 
