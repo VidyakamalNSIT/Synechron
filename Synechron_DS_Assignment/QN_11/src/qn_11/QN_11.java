a71eeb33f2eefd59b3bace4b4a4000da0b44df9e
Vidyakamal <31116133+VidyakamalNSIT@users.noreply.github.com>
2018-11-21 11:44:19 +0530

    Update QN_11.java

--- Synechron_DS_Assignment/QN_11/src/qn_11/QN_11.java
+++ Synechron_DS_Assignment/QN_11/src/qn_11/QN_11.java
@@ -9,13 +9,49 @@ package qn_11;
  *
  * @author Vidyakamal
  */
-public class QN_11 {
+import java.util.Scanner;
+class QN_11 
+{ 
+	static int binarySearch(int arr[], int l, int r, int x) 
+	{ 
+		if (r>=l) 
+		{ 
+			int mid = l + (r - l)/2; 
+			if (arr[mid] == x) 
+				return mid; 
+			if (arr[mid] > x) 
+				return binarySearch(arr, l, mid-1, x); 
+			return binarySearch(arr, mid+1, r, x); 
+		} 
+		return -1; 
+	} 
+	static int findPos(int arr[],int key)	 
+	{ 
+		int l = 0, h = 1; 
+		int val = arr[0]; 
+		while (val < key) 
+		{ 
+			l = h;	 
+			h = 2*h;	 
+			val = arr[h]; 
+		}  
+		return binarySearch(arr, l, h, key); 
+	} 
+	public static void main(String[] args) 
+	{ 
+	    Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); 
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+                arr[i]= sc.nextInt();
+        } 
+        int k = sc.nextInt();
+		int ans = findPos(arr,k); 
+		
+		if (ans==-1) 
+			System.out.println("Element not found"); 
+		else
+			System.out.println("Element found at index " + ans); 
+	} 
+} 
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
-}
