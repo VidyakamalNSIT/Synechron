6bc9b727249734fce77f84ae23ea5b4682950f55
Vidyakamal <31116133+VidyakamalNSIT@users.noreply.github.com>
2018-11-21 11:30:31 +0530

    Update QN_13.java

--- Synechron_DS_Assignment/QN_13/src/qn_13/QN_13.java
+++ Synechron_DS_Assignment/QN_13/src/qn_13/QN_13.java
@@ -9,13 +9,35 @@ package qn_13;
  *
  * @author Vidyakamal
  */
-public class QN_13 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
-}
+import java.util.Scanner;
+class QN_13 { 
+	void Sort(int arr[], int n){ 
+		int count = 0;
+		for (int i = 0; i < n; i++) { 
+			if (arr[i] == 0) 
+				count++; 
+		} 
+		for (int i = 0; i < count; i++) 
+			arr[i] = 0; 
+		for (int i = count; i < n; i++) 
+			arr[i] = 1; 
+	} void print(int arr[], int n) 
+	{ 
+		System.out.print("Array after segregation is "); 
+		for (int i = 0; i < n; i++) 
+			System.out.print(arr[i] + " ");	 
+	} 
+	public static void main(String[] args) 
+	{ 
+		QN_13 obj = new QN_13(); 
+    	Scanner sc = new Scanner(System.in);
+    	int n=sc.nextInt();
+    	int[] arr = new int[n];
+    	for(int i=0; i<n; i++){
+    	    arr[i]=sc.nextInt();
+    	}
+		obj.Sort(arr, n); 
+		System.out.println("Array after Sorting "); 
+		obj.print(arr, n); 
+	} 
+} 
